66d161a5a1df65620f5cd5ae39b2fdbaeefd0ba6
Illia Nesteruk <buk340@gmail.com>
2023-02-10 14:58:16 +0200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {visitAll} from "@angular/compiler";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'NgToDo';
+  counter = 0
+  updatedAt?: number
+
+
+
+  negativeCounters() {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.updatedAt = Date.now()
+    this.counter++
+  }
+
+  decrease(): void {
+    this.updatedAt = Date.now()
+    this.counter--
+  }
+
+  reset(): void {
+    this.updatedAt = Date.now()
+    this.counter = 0
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    RouterModule.forRoot([
+      {path:'**', redirectTo: ''}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
